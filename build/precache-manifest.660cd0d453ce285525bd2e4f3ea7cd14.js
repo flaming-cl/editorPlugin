292e051e81d9c75b3b00a69319150f77bca4bf7d
Q16 <lugina422@gmail.com>
2020-07-04 15:43:23 +0800

    first commit

---
+++ build/precache-manifest.660cd0d453ce285525bd2e4f3ea7cd14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6ab2ac3ca80446a88b00d421e52092",
+    "url": "/editorPlugin/index.html"
+  },
+  {
+    "revision": "b5459a0e3aa9a743b0b8",
+    "url": "/editorPlugin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb7418e50a14b73a96d5",
+    "url": "/editorPlugin/static/js/2.2d52a7c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/editorPlugin/static/js/2.2d52a7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5459a0e3aa9a743b0b8",
+    "url": "/editorPlugin/static/js/main.6c380883.chunk.js"
+  },
+  {
+    "revision": "fed80c3ca24aef93fd21",
+    "url": "/editorPlugin/static/js/runtime-main.58c0a02e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/editorPlugin/static/media/logo.5d5d9eef.svg"
+  }
+]);
