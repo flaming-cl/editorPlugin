266d30ca1acc3393e604fafa4eda2023e26e5050
flaming-cl <flaming-cl@hotmail.com>
2023-01-24 00:23:18 -0500

    <feat>v2.0.2

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ export const editorConfig =
                     colorCollections,
                     defaultColor,
                     type: 'text',
-                    customPicker: true
+                    customPicker: true,
                 }
             },
             header: {
